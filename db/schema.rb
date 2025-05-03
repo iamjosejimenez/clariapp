b1cf5a1ae3bf8348db550bf1826ee098ab5efd0f
JL Jiménez <jimenezbjosel@gmail.com>
2025-05-03 12:32:22 -0400

    migracion a postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_04_14_232328) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "goal_snapshots", force: :cascade do |t|
     t.integer "goal_id", null: false
     t.float "nav"
