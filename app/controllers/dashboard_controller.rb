60043575a29b7aae04e42563e04d460c2a82af0e
JL Jiménez <jimenezbjosel@gmail.com>
2025-05-03 15:40:28 -0400

    add total available to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,6 +6,7 @@ class DashboardController < ApplicationController
     user = User.find_by(email:)
     SyncGoalsService.new(user).call
     @goals = Goal.where(user_id: user.id)
+    @total_available = @goals.sum(:nav)
   end
 
   private
