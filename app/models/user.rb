6d66b5fbc1a6b1933a0838be2d8e197703a9ee0d
JL Jiménez <jimenezbjosel@gmail.com>
2025-05-03 21:41:36 -0400

    agregar indice sobre email users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,16 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 
 class User < ApplicationRecord
   encrypts :password
   encrypts :token
+
+  validates :email, presence: true, uniqueness: true
 
   has_many :goals, dependent: :destroy
 end
