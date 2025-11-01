5dfc5abeb1fbc3c3c3aa78726e5e36151ccc9011
JL Jiménez <jimenezbjosel@gmail.com>
2025-10-31 23:39:18 -0300

    add tests users to job

--- app/jobs/fetch_goal_snapshots_job.rb
+++ app/jobs/fetch_goal_snapshots_job.rb
@@ -8,5 +8,11 @@ class FetchGoalSnapshotsJob < ApplicationJob
       logger.info "Fetching goal snapshots for user #{user.id}"
       SyncGoalsService.new(user).call
     end
+
+    ExternalAccount.where(provider: "tests").find_each do |user|
+      user.goals.each do |goal|
+        FactoryBot.create(:goal_snapshot, goal: goal, extraction_date: Date.current)
+      end
+    end
   end
 end
